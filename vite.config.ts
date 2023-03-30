ea0fd662ce310c0429a5a34984c9c56adfa826c6
INSYDESW_TW\kenny.wu <kenny.wu@insyde.com>
2023-03-30 15:47:03 +0800

    Add package.json to every module to set up exported files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".")[1];
+          if (extType == "css") {
+            return `[name][extname]`;
+          }
+          return `Resources/[name][extname]`;
+        },
+      },
+    },
+  },
+});
