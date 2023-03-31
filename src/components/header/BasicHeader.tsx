9f6b20117096e27233cf58dee6aeaa560120ddff
INSYDESW_TW\kenny.wu <kenny.wu@insyde.com>
2023-03-31 13:51:01 +0800

    Add the BasicHeader component

---
+++ src/components/header/BasicHeader.tsx
@@ -0,0 +1,53 @@
+import { ComponentStyleMerging } from "../../metadata/ComponentStyle";
+import { MergeComponentStyle } from "../../utility/componentUtility";
+
+export interface BasicHeaderProps {
+  title?: React.ReactNode;
+  icon?: any;
+  containerStyle?: ComponentStyleMerging;
+  iconStyle?: ComponentStyleMerging;
+  titleStyle?: ComponentStyleMerging;
+}
+
+const BasicHeader: React.FC<BasicHeaderProps> = ({
+  title,
+  icon,
+  iconStyle,
+  containerStyle,
+  titleStyle,
+}) => {
+  const _containerStyle = MergeComponentStyle(
+    {
+      css: "flex items-center gap-3 justify-center w-full h-full",
+      style: {
+        backgroundColor: "#0e6eb8",
+      },
+    },
+    containerStyle
+  );
+
+  const _titleStyle = MergeComponentStyle(
+    {
+      css: "text-4xl text-white font-bold",
+      style: {
+        backgroundColor: "#0e6eb8",
+      },
+    },
+    titleStyle
+  );
+
+  console.log(_titleStyle);
+
+  return (
+    <div className={_containerStyle.css} style={_containerStyle.style}>
+      <div className={iconStyle?.css} style={iconStyle?.style}>
+        <img src={icon} alt="header icon" />
+      </div>
+      <div className={_titleStyle.css} style={_titleStyle.style}>
+        {title}
+      </div>
+    </div>
+  );
+};
+
+export default BasicHeader;
