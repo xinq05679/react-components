46f1c330f3e7978df05ff40b414e5bdd45ce08c3
INSYDESW_TW\kenny.wu <kenny.wu@insyde.com>
2023-04-26 12:02:39 +0800

    Modify the BasicTable component

--- src/components/table/BasicTable.tsx
+++ src/components/table/BasicTable.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { useLayoutEffect, useRef, useState } from "react";
 import { ComponentStyleMerging } from "../../metadata/ComponentStyle";
 import { MergeComponentStyle } from "../../utility/componentUtility";
@@ -21,45 +22,18 @@ export const BasicTable: React.FC<BasicTableProps> = ({
   rowStyle,
   cellStyle,
 }) => {
-  const [size, setSize] = useState([0, 0]);
-  const tableDivRef = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    const resizeHandler = () => {
-      const parentEl = tableDivRef.current?.parentElement;
-      if (parentEl) {
-        setSize([
-          parseInt(`${parentEl.clientWidth - 10}`),
-          parseInt(`${parentEl.clientHeight - 10}`),
-        ]);
-      } else {
-        setSize([0, 0]);
-      }
-    };
-
-    window.addEventListener("resize", resizeHandler);
-
-    window.dispatchEvent(new Event("resize"));
-
-    return () => {
-      window.removeEventListener("resize", resizeHandler);
-    };
-  }, [tableDivRef]);
-
   const _tableDivStyle = MergeComponentStyle(
     {
       css: `overflow-auto m-auto`,
-      style: {
-        width: size[0] ? `${size[0]}px` : "100%",
-        height: size[1] ? `${size[1]}px` : "100%",
-      },
     },
     tableDivStyle
   );
 
   const _tableStyle = MergeComponentStyle(
     {
-      css: "w-[100%] relative table-auto border-separate border-spacing-y-1",
+      css: classNames(
+        "w-[100%] relative table-auto border-separate border-spacing-y-1"
+      ),
     },
     tableStyle
   );
@@ -81,11 +55,7 @@ export const BasicTable: React.FC<BasicTableProps> = ({
   const _cellStyle = MergeComponentStyle({}, cellStyle);
 
   return (
-    <div
-      ref={tableDivRef}
-      className={_tableDivStyle.css}
-      style={_tableDivStyle.style}
-    >
+    <div className={_tableDivStyle.css} style={_tableDivStyle.style}>
       <table className={_tableStyle.css} style={_tableStyle.style}>
         <thead>
           {headerCell && (
