a51db8d5984f39343eabd069d3734a583b363a44
INSYDESW_TW\kenny.wu <kenny.wu@insyde.com>
2023-08-15 13:42:51 +0800

    Use forwoardRef on the BasicTextInpit and TextInput1 components

--- src/components/input/BasicTextInput.tsx
+++ src/components/input/BasicTextInput.tsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import { MergeComponentStyle } from "../../utility/componentUtility";
 import { ComponentStyleMerging } from "../../metadata/ComponentStyle";
-import { useState, useRef } from "react";
+import { useState, useRef, forwardRef, createRef } from "react";
 
 export interface BasicTextInputProps {
   value?: string;
@@ -17,106 +17,116 @@ export interface BasicTextInputProps {
   autoFocus?: boolean;
 }
 
-export const BasicTextInput: React.FC<BasicTextInputProps> = ({
-  value = "",
-  type = "text",
-  placeholder = "",
-  formStyle,
-  inputStyle,
-  enableSelectAll,
-  readOnly,
-  autoFocus,
-  onValueChanged,
-  onSubmit,
-  onFocus,
-}) => {
-  const [inputValue, setInputValue] = useState("");
-  const [submitValue, setSubmitValue] = useState("");
-  const inputRef = useRef<HTMLInputElement>(null);
-  const submitRef = useRef<HTMLInputElement>(null);
-
-  if (submitValue !== value) {
-    setInputValue(value);
-    setSubmitValue(value);
-  }
-
-  const _formStyle = MergeComponentStyle(
+export const BasicTextInput = forwardRef<HTMLInputElement, BasicTextInputProps>(
+  (
     {
-      css: classNames("flex items-center", "h-[32px] w-[100%]", "text-[16px]"),
+      value = "",
+      type = "text",
+      placeholder = "",
+      formStyle,
+      inputStyle,
+      enableSelectAll,
+      readOnly,
+      autoFocus,
+      onValueChanged,
+      onSubmit,
+      onFocus,
     },
-    formStyle
-  );
+    ref
+  ) => {
+    const [inputValue, setInputValue] = useState("");
+    const [submitValue, setSubmitValue] = useState("");
+    const submitRef = useRef<HTMLInputElement>(null);
 
-  const _inputStyle = MergeComponentStyle(
-    {
-      css: classNames(
-        [
-          "h-[100%]",
-          "outline-0",
-          "border border-[#888] rounded-md",
-          "cursor-pointer",
-          "pl-[5px]",
-          "grow",
-        ],
-        ["hover:border-[#00f]", "focus:border-[#00f]", "focus:cursor-auto"],
-        [
-          "[&[readOnly]]:bg-[#eee]",
-          "[&[readOnly]]:border-[#ddd]",
-          "[&[readOnly]]:text-[#888]",
-          "[&[readOnly]]:cursor-auto",
-        ],
-        // Placeholder Style
-        ["placeholder:text-[#888]", "placeholder:text-center"]
-      ),
-    },
-    inputStyle
-  );
-
-  const handleValueChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-    onValueChanged?.(event.target.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (submitValue !== inputValue) {
-      setSubmitValue(inputValue);
-      onSubmit?.(inputValue);
+    if (submitValue !== value) {
+      setInputValue(value);
+      setSubmitValue(value);
     }
-  };
 
-  return (
-    <form
-      className={_formStyle.css}
-      style={_formStyle.style}
-      onSubmit={handleSubmit}
-    >
-      <input
-        readOnly={readOnly}
-        ref={inputRef}
-        className={_inputStyle.css}
-        style={_inputStyle.style}
-        type={type}
-        placeholder={placeholder}
-        value={inputValue}
-        onChange={handleValueChanged}
-        onClick={() => {
-          if (enableSelectAll) inputRef.current?.select();
-        }}
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            inputRef.current?.blur();
-          }
-        }}
-        onBlur={() => {
-          submitRef.current?.click();
-        }}
-        onFocus={onFocus}
-        autoFocus={autoFocus}
-      />
-      <input ref={submitRef} type="submit" className="hidden" />
-    </form>
-  );
-};
+    const _formStyle = MergeComponentStyle(
+      {
+        css: classNames(
+          "flex items-center",
+          "h-[32px] w-[100%]",
+          "text-[16px]"
+        ),
+      },
+      formStyle
+    );
+
+    const _inputStyle = MergeComponentStyle(
+      {
+        css: classNames(
+          [
+            "h-[100%]",
+            "outline-0",
+            "border border-[#888] rounded-md",
+            "cursor-pointer",
+            "pl-[5px]",
+            "grow",
+          ],
+          ["hover:border-[#00f]", "focus:border-[#00f]", "focus:cursor-auto"],
+          [
+            "[&[readOnly]]:bg-[#eee]",
+            "[&[readOnly]]:border-[#ddd]",
+            "[&[readOnly]]:text-[#888]",
+            "[&[readOnly]]:cursor-auto",
+          ],
+          // Placeholder Style
+          ["placeholder:text-[#888]", "placeholder:text-center"]
+        ),
+      },
+      inputStyle
+    );
+
+    const handleValueChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(event.target.value);
+      onValueChanged?.(event.target.value);
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      if (submitValue !== inputValue) {
+        setSubmitValue(inputValue);
+        onSubmit?.(inputValue);
+      }
+    };
+
+    return (
+      <form
+        className={_formStyle.css}
+        style={_formStyle.style}
+        onSubmit={handleSubmit}
+      >
+        <input
+          readOnly={readOnly}
+          ref={ref}
+          className={_inputStyle.css}
+          style={_inputStyle.style}
+          type={type}
+          placeholder={placeholder}
+          value={inputValue}
+          onChange={handleValueChanged}
+          onClick={() => {
+            // @ts-ignore
+            if (enableSelectAll) ref?.current?.select();
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              // @ts-ignore
+              ref?.current?.blur();
+            }
+          }}
+          onBlur={() => {
+            submitRef.current?.click();
+          }}
+          onFocus={onFocus}
+          autoFocus={autoFocus}
+        />
+        <input ref={submitRef} type="submit" className="hidden" />
+      </form>
+    );
+  }
+);
 
 export default BasicTextInput;
--- src/components/input/TextInput1.tsx
+++ src/components/input/TextInput1.tsx
@@ -3,6 +3,7 @@ import { BiError } from "react-icons/bi";
 import { ComponentStyleMerging } from "../../..";
 import BasicTextInput, { BasicTextInputProps } from "./BasicTextInput";
 import { MergeComponentStyle } from "../../..";
+import { forwardRef } from "react";
 
 export interface TextInput1Props extends BasicTextInputProps {
   icon?: React.ReactNode;
@@ -13,85 +14,82 @@ export interface TextInput1Props extends BasicTextInputProps {
   errorMessageStyle?: ComponentStyleMerging;
 }
 
-export const TextInput1: React.FC<TextInput1Props> = ({
-  icon,
-  errorMessage,
-  iconDivStyle,
-  ...others
-}) => {
-  const _iconDivStyle = MergeComponentStyle(
-    {
-      css: classNames("absolute left-[10px]", "w-[24px]"),
-    },
-    iconDivStyle
-  );
+export const TextInput1 = forwardRef<HTMLInputElement, TextInput1Props>(
+  ({ icon, errorMessage, iconDivStyle, ...others }, ref) => {
+    const _iconDivStyle = MergeComponentStyle(
+      {
+        css: classNames("absolute left-[10px]", "w-[24px]"),
+      },
+      iconDivStyle
+    );
 
-  const _inputStyle = MergeComponentStyle(
-    {
-      css: classNames(
-        { "pl-[40px]": icon },
-        {
-          "bg-[#fef7ea]": errorMessage,
-          "border-[#f09b11]": errorMessage,
-          "hover:border-[#f09b11]": errorMessage,
-          "focus:border-[#f09b11]": errorMessage,
-        }
-      ),
-    },
-    // @ts-ignore
-    others.inputStyle
-  );
+    const _inputStyle = MergeComponentStyle(
+      {
+        css: classNames(
+          { "pl-[40px]": icon },
+          {
+            "bg-[#fef7ea]": errorMessage,
+            "border-[#f09b11]": errorMessage,
+            "hover:border-[#f09b11]": errorMessage,
+            "focus:border-[#f09b11]": errorMessage,
+          }
+        ),
+      },
+      // @ts-ignore
+      others.inputStyle
+    );
 
-  const _errorMessageStyle = MergeComponentStyle(
-    {
-      css: classNames(
-        "flex items-start",
-        "absolute bottom-[-30px]",
-        "h-[24px]",
-        "text-sm text-[#f00]/80 text-start"
-      ),
-    },
-    //@ts-ignore
-    others.errorMessageStyle
-  );
+    const _errorMessageStyle = MergeComponentStyle(
+      {
+        css: classNames(
+          "flex items-start",
+          "absolute bottom-[-30px]",
+          "h-[24px]",
+          "text-sm text-[#f00]/80 text-start"
+        ),
+      },
+      //@ts-ignore
+      others.errorMessageStyle
+    );
 
-  const _divStyle = MergeComponentStyle(
-    {
-      css: classNames("relative", " w-[100%]", "flex items-center"),
-    },
-    //@ts-ignore
-    others.divStyle
-  );
+    const _divStyle = MergeComponentStyle(
+      {
+        css: classNames("relative", " w-[100%]", "flex items-center"),
+      },
+      //@ts-ignore
+      others.divStyle
+    );
 
-  const _errorMessageIconStyle = MergeComponentStyle(
-    {
-      css: classNames("shrink-0", "me-1", "w-[18px] h-[18px]"),
-    },
-    //@ts-ignore
-    others.errorMessageIconStyle
-  );
+    const _errorMessageIconStyle = MergeComponentStyle(
+      {
+        css: classNames("shrink-0", "me-1", "w-[18px] h-[18px]"),
+      },
+      //@ts-ignore
+      others.errorMessageIconStyle
+    );
 
-  return (
-    <div className={classNames(_divStyle.css)} style={_divStyle.style}>
-      {/* Image */}
-      {icon && <div className={_iconDivStyle.css}>{icon}</div>}
-      {/* Text Input */}
-      <BasicTextInput {...others} inputStyle={_inputStyle} />
-      {/* Error Message */}
-      {errorMessage && (
-        <div
-          className={_errorMessageStyle.css}
-          style={_errorMessageStyle.style}
-        >
-          <BiError
-            className={_errorMessageIconStyle.css}
-            style={_errorMessageIconStyle.style}
-          />
-          {errorMessage}
-        </div>
-      )}
-    </div>
-  );
-};
+    return (
+      <div className={classNames(_divStyle.css)} style={_divStyle.style}>
+        {/* Image */}
+        {icon && <div className={_iconDivStyle.css}>{icon}</div>}
+        {/* Text Input */}
+        <BasicTextInput ref={ref} {...others} inputStyle={_inputStyle} />
+        {/* Error Message */}
+        {errorMessage && (
+          <div
+            className={_errorMessageStyle.css}
+            style={_errorMessageStyle.style}
+          >
+            <BiError
+              className={_errorMessageIconStyle.css}
+              style={_errorMessageIconStyle.style}
+            />
+            {errorMessage}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 export default TextInput1;
