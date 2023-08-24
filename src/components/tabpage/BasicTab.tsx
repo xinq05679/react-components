28f215eb4f5770de5985ee80117dff01f2d97b9f
INSYDESW_TW\kenny.wu <kenny.wu@insyde.com>
2023-08-24 14:16:18 +0800

    Change the text data type to React.ReactNode of the BasicTabProps

--- src/components/tabpage/BasicTab.tsx
+++ src/components/tabpage/BasicTab.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 export interface BasicTabProps {
   icon?: React.ReactNode;
-  text?: string;
+  text?: React.ReactNode;
   page?: React.ReactNode;
   isSelected?: boolean;
   isModified?: boolean;
